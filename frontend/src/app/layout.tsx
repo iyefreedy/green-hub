973f5d89ba167e8fd8365c4addaf36fdcb651966
Muhammad Quraisy <quraisy@uai.ac.id>
2024-08-02 21:29:30 +0700

    refactor: move folder assets

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,41 +3,48 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const author = localFont({
-  src: [
-    {
-      path: "./assets/Author-Light.otf",
-      weight: "300",
-      style: "italic",
-    },
-    {
-      path: "./assets/Author-Regular.otf",
-      weight: "400",
-    },
-    {
-      path: "./assets/Author-Medium.otf",
-      weight: "500",
-    },
-    {
-      path: "./assets/Author-Bold.otf",
-      weight: "700",
-    },
-  ],
+	src: [
+		{
+			path: "./../assets/fonts/Author-ExtraLight.otf",
+			weight: "200",
+		},
+		{
+			path: "./../assets/fonts/Author-Light.otf",
+			weight: "300",
+		},
+		{
+			path: "./../assets/fonts/Author-Regular.otf",
+			weight: "400",
+		},
+		{
+			path: "./../assets/fonts/Author-Medium.otf",
+			weight: "500",
+		},
+		{
+			path: "./../assets/fonts/Author-Semibold.otf",
+			weight: "600",
+		},
+		{
+			path: "./../assets/fonts/Author-Bold.otf",
+			weight: "700",
+		},
+	],
 });
 
 export const metadata: Metadata = {
-  title: "Green Hub",
-  description:
-    "Green Hub is a sustainable marketing community platform that connects eco-friendly businesses. Join to collaborate, share resources, and expand your network within the sustainable business ecosystem.",
+	title: "Green Hub",
+	description:
+		"Green Hub is a sustainable marketing community platform that connects eco-friendly businesses. Join to collaborate, share resources, and expand your network within the sustainable business ecosystem.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={author.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={author.className}>{children}</body>
+		</html>
+	);
 }
