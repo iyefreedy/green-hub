6530c2e97ea98afd914af17676a94d8d418923e5
ardy-jaya <ardy.henjaya@gmail.com>
2024-08-02 12:29:13 +0700

    update

---
+++ frontend/src/app/aboutUs/aboutus.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutUs: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello World!, this is me</h1>
+        </div>
+    );
+};
+
+export default AboutUs;
